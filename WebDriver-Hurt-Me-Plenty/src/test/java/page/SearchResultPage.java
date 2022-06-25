bafb87b2323432d6a0bb79058b84f9f56bded984
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-06-25 11:17:54 +0300

    Add SearchResultPage

--- WebDriver-Hurt-Me-Plenty/src/test/java/page/SearchResultPage.java
+++ WebDriver-Hurt-Me-Plenty/src/test/java/page/SearchResultPage.java
@@ -1,2 +1,35 @@
-package page;public class SearchResultPage {
+package page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+
+public class SearchResultPage {
+
+    private WebDriver driver;
+    private String searchTerm;
+
+    public SearchResultPage (WebDriver driver, String searchTerm) {
+        this.driver = driver;
+        this.searchTerm = searchTerm;
+    }
+
+    public GoogleCloudPricingCalculatorPage openGoogleCloudPricingCalculatorPage () {
+        List<WebElement> searchResults = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("a.gs-title")));
+        for (WebElement element : searchResults) {
+            if (element.getText().equalsIgnoreCase(searchTerm)) {
+                element.click();
+                break;
+            }
+        }
+        return new GoogleCloudPricingCalculatorPage(driver, searchTerm);
+    }
+
 }
