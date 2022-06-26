ec8588f0fb265d7349e2eddf62deed66758ae50b
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-06-26 18:15:49 +0300

    Move I Can Win files

--- src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/ICanWin/test/WebDriverPastebinTest.java
+++ src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/ICanWin/test/WebDriverPastebinTest.java
@@ -2,11 +2,11 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.PastebinHomePage;
+
 
 public class WebDriverPastebinTest {
 
