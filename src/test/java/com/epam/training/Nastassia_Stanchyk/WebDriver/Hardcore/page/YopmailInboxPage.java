4da8fd4b17d6e59f033ed693f491fa090cbd6f93
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-06-29 12:15:46 +0300

    Implemented Yopmail Mail Inbox Page class

--- src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/Hardcore/page/YopmailInboxPage.java
+++ src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/Hardcore/page/YopmailInboxPage.java
@@ -1,2 +1,33 @@
-package com.epam.training.Nastassia_Stanchyk.WebDriver.Hardcore.page;public class YopmailInboxPage {
+package com.epam.training.Nastassia_Stanchyk.WebDriver.Hardcore.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class YopmailInboxPage {
+
+    private WebDriver driver;
+
+    public YopmailInboxPage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String checkTotalMonthlyPrice () {
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.id("refresh")))
+                .click();
+        driver.switchTo().frame(new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("ifmail"))));
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("td[colspan='3'] + td > h3")))
+                .getText();
+    }
 }
