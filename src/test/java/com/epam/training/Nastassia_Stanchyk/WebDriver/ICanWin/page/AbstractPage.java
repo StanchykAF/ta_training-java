dafed612014e1f5cefbfd679340a5fdb2a0127a3
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-07-01 11:22:23 +0300

    Implement Abstract Page in I Can Win problem

--- src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/ICanWin/page/AbstractPage.java
+++ src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/ICanWin/page/AbstractPage.java
@@ -1,2 +1,28 @@
-package com.epam.training.Nastassia_Stanchyk.WebDriver.ICanWin.page;public class AbstractPage {
+package com.epam.training.Nastassia_Stanchyk.WebDriver.ICanWin.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class AbstractPage {
+
+    protected WebDriver driver;
+    
+    public AbstractPage (WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public AbstractPage acceptCookies () {
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[mode='primary']")))
+                .click();
+        return this;
+    }
+
+    public abstract AbstractPage createNewPaste(String hello_from_webDriver, String helloweb);
 }
