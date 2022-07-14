a526c6d323ea9e9a0723cd45357386a0f2a3628a
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-07-14 11:52:18 +0300

    Hurt Me Plenty: refactoring after 2nd code review

--- src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/HurtMePlenty/page/GoogleCloudPricingCalculatorPage.java
+++ src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/HurtMePlenty/page/GoogleCloudPricingCalculatorPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -17,46 +16,46 @@ import java.util.List;
 public class GoogleCloudPricingCalculatorPage extends AbstractPage {
 
     private final WebDriverWait WAIT = new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS));
-    private final String defaultDropdownOptionLocator = "//div[contains(@class, 'md-active')]" +
-        "//md-option/div[contains(text(), '%s')]";
+    private static final String DEFAULT_DROPDOWN_OPTION_LOCATOR = "//div[contains(@class, 'md-active')]" +
+            "//md-option/div[contains(text(), '%s')]";
 
-    @FindBy (css = "div.compute[title='Compute Engine']")
+    @FindBy(css = "div.compute[title='Compute Engine']")
     private WebElement productName;
 
-    @FindBy (css = "form[name='ComputeEngineForm'] > div > div > md-input-container > input[name='quantity']")
+    @FindBy(css = "form[name='ComputeEngineForm'] > div > div > md-input-container > input[name='quantity']")
     private WebElement numberOfInstancesInput;
 
-    @FindBy (css = "md-select[aria-label^='Operating System']")
+    @FindBy(css = "md-select[aria-label^='Operating System']")
     private WebElement operatingSystemDropdown;
 
-    @FindBy (css = "md-select[aria-label^='VM Class']")
+    @FindBy(css = "md-select[aria-label^='VM Class']")
     private WebElement VMClassDropdown;
 
-    @FindBy (css = "md-select[aria-label^='Series']")
+    @FindBy(css = "md-select[aria-label^='Series']")
     private WebElement instanceSeriesDropdown;
 
-    @FindBy (css = "md-select[aria-label^='Instance type']")
+    @FindBy(css = "md-select[aria-label^='Instance type']")
     private WebElement instanceTypeDropdown;
 
-    @FindBy (css = "md-checkbox[ng-model='listingCtrl.computeServer.addGPUs'] > div.md-container.md-ink-ripple")
+    @FindBy(css = "md-checkbox[ng-model='listingCtrl.computeServer.addGPUs'] > div.md-container.md-ink-ripple")
     private WebElement addGPUsCheckBox;
 
-    @FindBy (css = "md-select[aria-label^='GPU type']")
+    @FindBy(css = "md-select[aria-label^='GPU type']")
     private WebElement GPUTypeDropdown;
 
-    @FindBy (css = "md-select[aria-label^='Number of GPUs']")
+    @FindBy(css = "md-select[aria-label^='Number of GPUs']")
     private WebElement numberOfGPUsDropdown;
 
-    @FindBy (css = "md-select[aria-label^='Local SSD']")
+    @FindBy(css = "md-select[aria-label^='Local SSD']")
     private WebElement localSSDDropdown;
 
-    @FindBy (css = "md-select[aria-label^='Datacenter location']")
+    @FindBy(css = "md-select[aria-label^='Datacenter location']")
     private WebElement datacenterLocationDropdown;
 
-    @FindBy (css = "md-select[aria-label^='Committed usage']")
+    @FindBy(css = "md-select[aria-label^='Committed usage']")
     private WebElement committedUsageDropdown;
 
-    @FindBy (css = "form[name='ComputeEngineForm'] > div > button")
+    @FindBy(css = "form[name='ComputeEngineForm'] > div > button")
     private WebElement addToEstimateButton;
 
     private final By mainFrame = By.cssSelector("#cloud-site > devsite-iframe > iframe");
@@ -64,12 +63,12 @@ public class GoogleCloudPricingCalculatorPage extends AbstractPage {
     private final By resultBlock = By.cssSelector("div.md-list-item-text");
     private final By totalMonthlyPrice = By.cssSelector("#resultBlock > md-card > md-card-content > div > div > div > h2 > b");
 
-    public GoogleCloudPricingCalculatorPage (WebDriver driver) {
+    public GoogleCloudPricingCalculatorPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
 
-    public GoogleCloudPricingCalculatorPage calculatePrice (FormData formData) {
+    public GoogleCloudPricingCalculatorPage calculatePrice(FormData formData) {
 
         driver.switchTo().frame(WAIT
                 .until(ExpectedConditions.presenceOfElementLocated(mainFrame)));
@@ -91,7 +90,7 @@ public class GoogleCloudPricingCalculatorPage extends AbstractPage {
         return this;
     }
 
-    public List<String> getCalculatedForm () {
+    public List<String> getCalculatedForm() {
         List<String> calculatedFormText = new ArrayList<>();
         List<WebElement> calculatedForm =
                 WAIT.until(ExpectedConditions.presenceOfAllElementsLocatedBy(resultBlock));
@@ -101,67 +100,68 @@ public class GoogleCloudPricingCalculatorPage extends AbstractPage {
         return calculatedFormText;
     }
 
-    public String getTotalMonthlyPrice () {
+    public String getTotalMonthlyPrice() {
         return WAIT.until(ExpectedConditions.presenceOfElementLocated(totalMonthlyPrice)).getText();
     }
 
-    public GoogleCloudPricingCalculatorPage selectOperatingSystem (String operatingSystem) {
+    public GoogleCloudPricingCalculatorPage selectOperatingSystem(String operatingSystem) {
         operatingSystemDropdown.click();
         selectDropdownOption(operatingSystem);
         return this;
     }
 
-    public GoogleCloudPricingCalculatorPage selectVMClass (String VMClass) {
+    public GoogleCloudPricingCalculatorPage selectVMClass(String VMClass) {
         VMClassDropdown.click();
         selectDropdownOption(VMClass);
         return this;
     }
 
-    public GoogleCloudPricingCalculatorPage selectInstanceSeries (String series) {
+    public GoogleCloudPricingCalculatorPage selectInstanceSeries(String series) {
         instanceSeriesDropdown.click();
         selectDropdownOption(series);
         return this;
     }
 
-    public GoogleCloudPricingCalculatorPage selectInstanceType (String instanceType) {
+    public GoogleCloudPricingCalculatorPage selectInstanceType(String instanceType) {
         instanceTypeDropdown.click();
         selectDropdownOption(instanceType);
         return this;
     }
-    public GoogleCloudPricingCalculatorPage selectGPUType (String GPUType) {
+
+    public GoogleCloudPricingCalculatorPage selectGPUType(String GPUType) {
         GPUTypeDropdown.click();
         selectDropdownOption(GPUType);
         return this;
     }
 
-    public GoogleCloudPricingCalculatorPage selectNumberOfGPUs (String numberOfGPUs) {
+    public GoogleCloudPricingCalculatorPage selectNumberOfGPUs(String numberOfGPUs) {
         numberOfGPUsDropdown.click();
         selectDropdownOption(numberOfGPUs);
         return this;
     }
 
-    public GoogleCloudPricingCalculatorPage selectLocalSSD (String localSSD) {
+    public GoogleCloudPricingCalculatorPage selectLocalSSD(String localSSD) {
         localSSDDropdown.click();
         selectDropdownOption(localSSD);
         return this;
     }
 
-    public GoogleCloudPricingCalculatorPage selectDatacenterLocation (String datacenterLocation) {
+    public GoogleCloudPricingCalculatorPage selectDatacenterLocation(String datacenterLocation) {
         datacenterLocationDropdown.click();
         selectDropdownOption(datacenterLocation);
         return this;
     }
 
-    public GoogleCloudPricingCalculatorPage selectCommittedUsageTime (String committedUsage) {
+    public GoogleCloudPricingCalculatorPage selectCommittedUsageTime(String committedUsage) {
         committedUsageDropdown.click();
         selectDropdownOption(committedUsage);
         return this;
     }
 
-    private void selectDropdownOption (String option) {
+    private void selectDropdownOption(String option) {
         WebElement dropdownOption =
-                WAIT.until(ExpectedConditions.presenceOfElementLocated(By.xpath(String.format(defaultDropdownOptionLocator,
-                option))));
+                WAIT.until(ExpectedConditions.presenceOfElementLocated(By.xpath(String.format(DEFAULT_DROPDOWN_OPTION_LOCATOR,
+                        option))));
         dropdownOption.click();
         WAIT.until(ExpectedConditions.invisibilityOf(dropdownOption));
     }
--- src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/HurtMePlenty/test/GoogleCloudPricingCalculatorTest.java
+++ src/test/java/com/epam/training/Nastassia_Stanchyk/WebDriver/HurtMePlenty/test/GoogleCloudPricingCalculatorTest.java
@@ -18,14 +18,14 @@ public class GoogleCloudPricingCalculatorTest {
     private List<String> actualPricingResultsForm;
     private String actualTotalMonthlyPrice;
 
-    @BeforeTest ()
-    private void browserSetup () {
+    @BeforeTest()
+    private void browserSetup() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
-    @BeforeTest (description = "Getting full calculated form")
-    private void createCalculatedForm () {
+    @BeforeTest(description = "Getting full calculated form")
+    private void createCalculatedForm() {
         String searchTerm = "Google Cloud Platform Pricing Calculator";
         GoogleCloudPricingCalculatorPage actualPricingResults;
         FormData formData = new FormData();
@@ -48,53 +48,53 @@ public class GoogleCloudPricingCalculatorTest {
         actualTotalMonthlyPrice = actualPricingResults.getTotalMonthlyPrice();
     }
 
-    @Test (description = "Checking VM class")
-    public void checkVMClassTest () {
+    @Test(description = "Checking VM class")
+    public void checkVMClassTest() {
         Assert.assertTrue(
                 searchInCalculatedForm("Provisioning model").contains("Regular"),
                 "Issue in VM Class field: actual result is \n" + searchInCalculatedForm("Provisioning model")
         );
     }
 
-    @Test (description = "Checking instance type")
-    public void checkInstanceTypeTest () {
+    @Test(description = "Checking instance type")
+    public void checkInstanceTypeTest() {
         Assert.assertTrue(
                 searchInCalculatedForm("Instance type").contains("n1-standard-8"),
                 "Issue in Instance Type field: actual result is \n" + searchInCalculatedForm("Instance type")
         );
     }
 
-    @Test (description = "Checking local SSD")
-    public void checkLocalSSDTest () {
+    @Test(description = "Checking local SSD")
+    public void checkLocalSSDTest() {
         Assert.assertTrue(
                 searchInCalculatedForm("Local SSD").contains("2x375 GiB"),
                 "Issue in Local SSD field: actual result is \n" + searchInCalculatedForm("Local SSD")
         );
     }
 
-    @Test (description = "Checking datacenter location")
-    public void checkDatacenterLocationTest () {
+    @Test(description = "Checking datacenter location")
+    public void checkDatacenterLocationTest() {
         Assert.assertTrue(
                 searchInCalculatedForm("Region").contains("Frankfurt"),
                 "Issue in Datacenter Location field: actual result is \n" + searchInCalculatedForm("Region")
         );
     }
 
-    @Test (description = "Checking committed usage term")
-    public void checkCommitmentTermTest () {
+    @Test(description = "Checking committed usage term")
+    public void checkCommitmentTermTest() {
         Assert.assertTrue(
                 searchInCalculatedForm("Commitment term").contains("1 Year"),
                 "Issue in Commitment Term field: actual result is \n" + searchInCalculatedForm("Commitment term")
         );
     }
 
-    @Test (description = "Checking total price per month")
-    public void checkTotalPricePerMonthTest () {
+    @Test(description = "Checking total price per month")
+    public void checkTotalPricePerMonthTest() {
         Assert.assertTrue(actualTotalMonthlyPrice.contains("USD 1,081.20"),
                 "Issue in Total Estimated Cost: actual result is \n" + actualTotalMonthlyPrice);
     }
 
-    private String searchInCalculatedForm (String targetString) {
+    private String searchInCalculatedForm(String targetString) {
         for (String string : actualPricingResultsForm) {
             if (string.startsWith(targetString)) {
                 return string;
@@ -103,8 +103,8 @@ public class GoogleCloudPricingCalculatorTest {
         return "Not found";
     }
 
-    @AfterTest (alwaysRun = true)
-    private void browserTearDown () {
+    @AfterTest(alwaysRun = true)
+    private void browserTearDown() {
         driver.quit();
         driver = null;
     }
